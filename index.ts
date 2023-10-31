9a0c1272c3ae680a72f1eac5e2a315b873280a89
eshodiev <eshodiev@cdti.uz>
2023-10-31 22:18:29 +0500

    31.10.2023

--- index.ts
+++ index.ts
@@ -3,5 +3,9 @@ import { TreeNode, fromArrayToTree } from "./helpers/treeNode";
 import { getMinimumDifference } from "./solutions/getMinimumDifference";
 
 
+// function diameterOfBinaryTree(root: TreeNode | null): number {
+
+// };
+
 
 execute(() => getMinimumDifference(fromArrayToTree([4,2,6,1,3])))
