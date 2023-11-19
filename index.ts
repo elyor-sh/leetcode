7a2406db2eeb5d96f8c75e9230882c48dc433b6e
Elyor Shodiyorov <elyor.sh.99@mail.ru>
2023-11-19 23:25:53 +0500

    19.11.2023

--- index.ts
+++ index.ts
@@ -1,4 +1,19 @@
 import { execute } from "./helpers/performance";
-import {longestSubstring} from "./solutions/longestSubstring";
 
-execute(() => longestSubstring("ababbc", 2))
+function findPairs(nums: number[], k: number): number {
+    const map = new Map<number, number>()
+    let count = 0
+
+    for (const num of nums){
+        map.set(num, (map.get(num) || 0) + 1)
+    }
+
+    for (const [key, value] of map){
+        if(map.get(key + k)){
+            count += (k !== 0 || value > 1) ? 1 : 0
+        }
+    }
+
+    return count
+}
+execute(() => findPairs( [1,1,1,2,2], 0))
