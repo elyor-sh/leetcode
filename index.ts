8571d3bcf6539c2c2b9e1f224d29e12b1b6f78d0
eshodiev <elyor.sh.99@mail.ru>
2024-01-15 20:19:03 +0500

    15.01.2024

--- index.ts
+++ index.ts
@@ -1,34 +1,6 @@
+import { findRedundantConnection } from "./solutions/684.findRedundantConnection";
 import { execute } from "./helpers/performance";
-import {findChampion} from "./solutions/2924.findChampion";
 
-function find(edge:number,parents:Array<number>){
-  if(parents[edge]===edge){
-      return edge;
-  }
-      
-  return find(parents[edge],parents)   
-}
-function findRedundantConnection(edges: number[][]): number[] {
-  let n=edges.length;
-  const parents=new Array(n+1);
-  
-  for(let index=0;index<n;index++){
-       parents[index]=index
-  }
-  
-  for(const edge of edges){
-      let u=find(edge[0],parents)
-      let v=find(edge[1],parents)
 
-      if(u===v){
-          return edge
-          
-      }
-      parents[v]=u
-      
-  }
-  
-  return []
-};
 
 execute(() => findRedundantConnection([[1,2],[1,3],[2,3]]));
